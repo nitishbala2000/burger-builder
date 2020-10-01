38fa18f7f3b604a42e5a42bb3acf5b9bff49b6fc
nitish <nitishbala2000@gmail.com>
2020-10-02 00:38:20 +0100

    Updates

---
+++ precache-manifest.f37f1dcb3f084f1f00e4936f7d8a816d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9235e3044a12d365a9fbbc8a39e6052f",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "bac0a5651d0a6f1e321f",
+    "url": "/burger-builder/static/css/main.069f60c0.chunk.css"
+  },
+  {
+    "revision": "4669cfca758da8ec5989",
+    "url": "/burger-builder/static/js/2.e9e4607b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.e9e4607b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac0a5651d0a6f1e321f",
+    "url": "/burger-builder/static/js/main.1e729007.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
